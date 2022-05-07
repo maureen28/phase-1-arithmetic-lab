7f489a79fb48532eaf99a84ec8bc7d144550be4a
maureen <maureen.murimi@moringaschool.com>
2022-05-07 15:52:12 +0300

    math functions added

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 58;
+let num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(5,10,15,20)
+
+
+
